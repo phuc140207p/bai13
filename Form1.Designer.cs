4a132df8d7b6b68b2923d3bf45beeda7c3c990cc
ADMIN <ADMIN@LAB1-MAY01>
2023-09-25 10:26:21 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace bai13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbllaptrinh = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radBlack = new System.Windows.Forms.RadioButton();
+            this.radBlue = new System.Windows.Forms.RadioButton();
+            this.RadGreen = new System.Windows.Forms.RadioButton();
+            this.RadRed = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkGachChan = new System.Windows.Forms.CheckBox();
+            this.chkltalic = new System.Windows.Forms.CheckBox();
+            this.chkBold = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtlaptrinh = new System.Windows.Forms.TextBox();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nhập tên";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbllaptrinh
+            // 
+            this.lbllaptrinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllaptrinh.Location = new System.Drawing.Point(146, 365);
+            this.lbllaptrinh.Name = "lbllaptrinh";
+            this.lbllaptrinh.Size = new System.Drawing.Size(203, 26);
+            this.lbllaptrinh.TabIndex = 8;
+            this.lbllaptrinh.TextChanged += new System.EventHandler(this.lbllaptrinh_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.radBlack);
+            this.groupBox1.Controls.Add(this.radBlue);
+            this.groupBox1.Controls.Add(this.RadGreen);
+            this.groupBox1.Controls.Add(this.RadRed);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(34, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(169, 172);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "color";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radBlack
+            // 
+            this.radBlack.AutoSize = true;
+            this.radBlack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radBlack.ForeColor = System.Drawing.Color.Black;
+            this.radBlack.Location = new System.Drawing.Point(34, 121);
+            this.radBlack.Name = "radBlack";
+            this.radBlack.Size = new System.Drawing.Size(66, 24);
+            this.radBlack.TabIndex = 4;
+            this.radBlack.TabStop = true;
+            this.radBlack.Text = "Black";
+            this.radBlack.UseVisualStyleBackColor = true;
+            // 
+            // radBlue
+            // 
+            this.radBlue.AutoSize = true;
+            this.radBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radBlue.ForeColor = System.Drawing.Color.Blue;
+            this.radBlue.Location = new System.Drawing.Point(34, 91);
+            this.radBlue.Name = "radBlue";
+            this.radBlue.Size = new System.Drawing.Size(59, 24);
+            this.radBlue.TabIndex = 3;
+            this.radBlue.TabStop = true;
+            this.radBlue.Text = "Blue";
+            this.radBlue.UseVisualStyleBackColor = true;
+            this.radBlue.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // RadGreen
+            // 
+            this.RadGreen.AutoSize = true;
+            this.RadGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadGreen.ForeColor = System.Drawing.Color.Lime;
+            this.RadGreen.Location = new System.Drawing.Point(34, 61);
+            this.RadGreen.Name = "RadGreen";
+            this.RadGreen.Size = new System.Drawing.Size(72, 24);
+            this.RadGreen.TabIndex = 2;
+            this.RadGreen.TabStop = true;
+            this.RadGreen.Text = "Green";
+            this.RadGreen.UseVisualStyleBackColor = true;
+            this.RadGreen.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // RadRed
+            // 
+            this.RadRed.AutoSize = true;
+            this.RadRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadRed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.RadRed.Location = new System.Drawing.Point(34, 31);
+            this.RadRed.Name = "RadRed";
+            this.RadRed.Size = new System.Drawing.Size(57, 24);
+            this.RadRed.TabIndex = 1;
+            this.RadRed.TabStop = true;
+            this.RadRed.Text = "Red";
+            this.RadRed.UseVisualStyleBackColor = true;
+            this.RadRed.CheckedChanged += new System.EventHandler(this.RadRed_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.chkGachChan);
+            this.groupBox2.Controls.Add(this.chkltalic);
+            this.groupBox2.Controls.Add(this.chkBold);
+            this.groupBox2.Location = new System.Drawing.Point(274, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(169, 172);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "font";
+            // 
+            // chkGachChan
+            // 
+            this.chkGachChan.AutoSize = true;
+            this.chkGachChan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkGachChan.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.chkGachChan.Location = new System.Drawing.Point(29, 110);
+            this.chkGachChan.Name = "chkGachChan";
+            this.chkGachChan.Size = new System.Drawing.Size(109, 24);
+            this.chkGachChan.TabIndex = 7;
+            this.chkGachChan.Text = "Gạch Chân";
+            this.chkGachChan.UseVisualStyleBackColor = true;
+            // 
+            // chkltalic
+            // 
+            this.chkltalic.AutoSize = true;
+            this.chkltalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkltalic.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.chkltalic.Location = new System.Drawing.Point(29, 71);
+            this.chkltalic.Name = "chkltalic";
+            this.chkltalic.Size = new System.Drawing.Size(136, 24);
+            this.chkltalic.TabIndex = 6;
+            this.chkltalic.Text = "Nghiêng ltalic";
+            this.chkltalic.UseVisualStyleBackColor = true;
+            // 
+            // chkBold
+            // 
+            this.chkBold.AutoSize = true;
+            this.chkBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBold.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.chkBold.Location = new System.Drawing.Point(29, 30);
+            this.chkBold.Name = "chkBold";
+            this.chkBold.Size = new System.Drawing.Size(106, 24);
+            this.chkBold.TabIndex = 5;
+            this.chkBold.Text = "Đậm Bold";
+            this.chkBold.UseVisualStyleBackColor = true;
+            this.chkBold.CheckedChanged += new System.EventHandler(this.chkBold_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(30, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lập Trình bởi";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtlaptrinh
+            // 
+            this.txtlaptrinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlaptrinh.Location = new System.Drawing.Point(169, 45);
+            this.txtlaptrinh.Name = "txtlaptrinh";
+            this.txtlaptrinh.Size = new System.Drawing.Size(209, 26);
+            this.txtlaptrinh.TabIndex = 0;
+            this.txtlaptrinh.TextChanged += new System.EventHandler(this.lbllaptrinh_TextChanged);
+            // 
+            // btThoat
+            // 
+            this.btThoat.Location = new System.Drawing.Point(379, 349);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(113, 56);
+            this.btThoat.TabIndex = 9;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 450);
+            this.Controls.Add(this.btThoat);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtlaptrinh);
+            this.Controls.Add(this.lbllaptrinh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lbllaptrinh;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton RadRed;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radBlack;
+        private System.Windows.Forms.RadioButton radBlue;
+        private System.Windows.Forms.RadioButton RadGreen;
+        private System.Windows.Forms.CheckBox chkGachChan;
+        private System.Windows.Forms.CheckBox chkltalic;
+        private System.Windows.Forms.CheckBox chkBold;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtlaptrinh;
+        private System.Windows.Forms.Button btThoat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            RadRed.Checked = true;
+            RadRed.ForeColor = Color.Red;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbllaptrinh_TextChanged(object sender, EventArgs e)
+        {
+            txtlaptrinh.Text = txtlaptrinh.Text;
+            lbllaptrinh.Text = txtlaptrinh.Text;
+        }
+
+        private void btThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RadRed_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton r = (RadioButton)sender;
+            switch (r.Name)
+            {
+                case "radRed":
+                    txtlaptrinh.ForeColor = Color.Red;
+                    lbllaptrinh.ForeColor = Color.Red;
+                    break;
+                case "radGreen":
+                    txtlaptrinh.ForeColor = Color.Green;
+                    lbllaptrinh.ForeColor = Color.Green;
+                    break;
+                case "radBlue":
+                    txtlaptrinh.ForeColor = Color.Blue;
+                    lbllaptrinh.ForeColor = Color.Blue;
+                    break;
+                case "radBack":
+                    txtlaptrinh.ForeColor = Color.Black;
+                    lbllaptrinh.ForeColor = Color.Black;
+                    break;
+            }
+            
+        }
+
+        private void chkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox r = (CheckBox)sender;
+            switch (r.Name)
+            {
+                case "radRed":
+                    txtlaptrinh.ForeColor = Color.Red;
+                    lbllaptrinh.ForeColor = Color.Red;
+                    break;
+                case "radGreen":
+                    txtlaptrinh.ForeColor = Color.Green;
+                    lbllaptrinh.ForeColor = Color.Green;
+                    break;
+                case "radBlue":
+                    txtlaptrinh.ForeColor = Color.Blue;
+                    lbllaptrinh.ForeColor = Color.Blue;
+                    break;
+            }
+        }
+    }
+}
